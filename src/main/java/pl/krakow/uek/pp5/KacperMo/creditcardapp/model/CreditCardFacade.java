71083c15008421cc7cdf26cf71da8318fc0eb560
Piotr Rachwalik <piotr.rachwalik@gmail.com>
2019-11-19 15:52:30 +0100

    get rid of NPE and some refactor

--- src/main/java/pl/krakow/uek/pp5/KacperMo/creditcardapp/model/CreditCardFacade.java
+++ src/main/java/pl/krakow/uek/pp5/KacperMo/creditcardapp/model/CreditCardFacade.java
@@ -3,7 +3,16 @@ package pl.krakow.uek.pp5.jkan.creditcardapp.model;
 import java.math.BigDecimal;
 
 public class CreditCardFacade {
+    private final InMemoryCCStorage storage;
 
+    CreditCardFacade(InMemoryCCStorage ccStorage) {
+        this.storage = ccStorage;
+    }
+    public void withdrawFromCard(String creditCardNumber, BigDecimal withdrawValue) {
+        CreditCard loaded = storage.load(creditCardNumber);
+        loaded.withdraw(withdrawValue);
+        storage.add(loaded);
+    }
 
     
 }
--- src/main/java/pl/krakow/uek/pp5/KacperMo/creditcardapp/model/InMemoryCCStorage.java
+++ src/main/java/pl/krakow/uek/pp5/KacperMo/creditcardapp/model/InMemoryCCStorage.java
@@ -1,15 +1,14 @@
 package pl.krakow.uek.pp5.jkan.creditcardapp.model;
 
+import pl.krakow.uek.pp5.KacperMo.creditcardapp.model.CreditCardStorage;
+
 import java.util.HashMap;
 
-class InMemoryCCStorage implements CreditCardStorage {
-
+public class InMemoryCCStorage implements CreditCardStorage {
     private HashMap<String, CreditCard> cards;
-
     InMemoryCCStorage() {
         cards = new HashMap<String, CreditCard>();
     }
-
     public void add(CreditCard card) {
         cards.put(card.cardNumber, card);
     }
