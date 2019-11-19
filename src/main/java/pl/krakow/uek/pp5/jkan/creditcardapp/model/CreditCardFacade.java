f3f5b4bc1e7d25abfbcc6256a4d226ac8ffedda7
Kaco <mail>
2019-11-19 14:07:03 +0100

    .

--- src/main/java/pl/krakow/uek/pp5/jkan/creditcardapp/model/CreditCardFacade.java
+++ src/main/java/pl/krakow/uek/pp5/jkan/creditcardapp/model/CreditCardFacade.java
@@ -4,17 +4,6 @@ import java.math.BigDecimal;
 
 public class CreditCardFacade {
 
-    private final CreditCardStorage storage;
 
-    CreditCardFacade(CreditCardStorage ccStorage) {
-        this.storage = ccStorage;
-    }
-
-    public void withdrawFromCard(String creditCardNumber, BigDecimal withdrawValue) {
-        CreditCard loaded = storage.load(creditCardNumber);
-
-        loaded.withdraw(withdrawValue);
-
-        storage.add(loaded);
-    }
+    
 }
